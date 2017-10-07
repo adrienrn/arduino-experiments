272ae127b3dc9ec7599ab28c8816790aed5df6fb
Hussard <adrien.ricartnoblet@gmail.com>
2017-10-07 18:02:33 +0200

    Using some ncurses for fun and display a menu / exit

--- std/src/main.c
+++ std/src/main.c
@@ -3,28 +3,85 @@
 #include <cstdio>
 #include <cstring>
 #include <unistd.h>
+#include <curses.h>
 
 using namespace std;
 
+struct Menu_S;
+
 typedef struct Menu_Item_S {
-    char*       text;
+  char*           text;
+  Menu_S*         menu;
+  void            (*callback)(int o);
 } Menu_Item_T;
 
-Menu_Item_S* menuItems;
+typedef struct {
+  char*               prompt;
+  int                 nbItems;
+  Menu_Item_S**       items;
+} Menu_T;
+
+const int MENU_COUNT = 1;
+const int MENU_ITEMS_COUNT = 5;
+const int MENU_ITEMS_TEXT_MAX = 16;
+
+Menu_T* appMenu = (Menu_T*) malloc(MENU_COUNT * sizeof(Menu_T));
+
+void doMenuAction(int selectedMenuItem)
+{
+    printf("%d\n", selectedMenuItem);
+}
 
 int setup()
 {
-    menuItems = (Menu_Item_S*) malloc(sizeof(Menu_Item_S));
+    Menu_Item_T** items = (Menu_Item_T**) malloc(MENU_ITEMS_COUNT * sizeof(Menu_Item_T*));
+    for (int i = 0; i < MENU_ITEMS_COUNT; ++i) {
+        items[i] = (Menu_Item_S*) malloc(sizeof(Menu_Item_T));
 
-    menuItems->text = "Oyo";
+        items[i]->text = (char*) malloc(MENU_ITEMS_TEXT_MAX * sizeof(char));
+        sprintf(items[i]->text, "ITEM %d", i);
+        items[i]->callback = &doMenuAction;
+    }
+
+    for (int h = 0; h < MENU_COUNT; ++h) {
+        appMenu = (Menu_T*) malloc(sizeof(Menu_T));
+
+        appMenu->prompt = "Choose:";
+        appMenu->nbItems = MENU_ITEMS_COUNT;
+        appMenu->items = items;
+    }
 
     return 0;
 }
 
 int loop()
 {
-    printf("'%s'\n", menuItems->text);
-    sleep(5);
+    char buffer[100];
+
+    initscr();
+    //(void)echo();
+
+    addstr("What is your name> ");
+
+    refresh();
+    getnstr(buffer, sizeof(buffer) - 1);
+
+    clear();
+
+    // printw("Greetings and salutations %s!\n", buffer);
+    printw("%s\n", appMenu->prompt);
+    for (int i = 0; i < appMenu->nbItems; ++i) {
+        printw("%s\n", appMenu->items[i]->text);
+    }
+    refresh();
+
+    //mvaddch(LINES - 1, COLS - 1, '.');
+    printw("\n\n\nPress ENTER to quit.");
+    refresh();
+
+    //getnstr(buffer, sizeof(buffer) - 1);
+    while (10 != getch());
+    endwin();
 
     return 0;
 }
@@ -34,7 +91,10 @@ int main(int argc, char *argv[])
     setup();
 
     for (;;) {
-        loop();
+        int exitValue = loop();
+        if (exitValue == 0) {
+            break;
+        }
     }
 
     return 0;
